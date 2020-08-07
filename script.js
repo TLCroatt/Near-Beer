36d916f439415a66dec3a2e6078d4809a717fd45
Brian I <brianingram78@gmail.com>
2020-08-06 20:00:39 -0600

    added saved list functionality

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
-$("#search").on("click", function() {
-    console.log("here")
-    event.preventDefault();
-    event.stopPropagation();
-    let city ="Denver" //$("#city-input").val().trim();
-    if (city !== "") {
-        // The following clears the error if something is typed in the search field that isn't accepted
-       $("#city-input").html("")
-        
-        // console.log(localStorage.getItem("city"));
-        // searchCity(city);
-        // brewery(city);
-        // addHistory(city);
-        // renderHistory();
+//global variables
+var city = "";
+var cities = [];
+var text = "";
+var searchType = "";
 
-        $.ajax({
-            url: createBreweryURL(),
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-        })
-        
-        //clear search results on new search
-        //$("")
-    }
-    else {
-        $("#city-input").html("Field cannot be empty");
-    }
-});
+const displayNum = 3;
+
 // Save the cities searched for
 function addHistory(city){ 
     // Check for changes in the local item and log them
-    cities.push(city);
-    localStorage.setItem("cities", cities); 
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    if(storedCities !== null){
+        cities = storedCities;
+    }
+
+    city.toUpperCase();
+console.log("city =" +city);
+    var index = cities.indexOf(city);
+console.log("index = " + index);
+    if(index === -1){
+        cities.push(city);
+        localStorage.setItem("cities", JSON.stringify(cities));
+    };
+    renderHistory();
+
 };
 
 // Render the history localstorage
 function renderHistory(){
     $("#history").empty();
-    for (i = 0; i < cities.length; i++) {
-        //    
-        $("#history").append($("<button class='btn btn-info d-flex flex-column'>").attr("cityName", cities[i]).text(cities[i]));
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    if(storedCities !== null){
+        cities = storedCities;
     }
-    $("#history button").on("click",function(){
-        event.preventDefault();
-        let searchedCity = $(this).attr("cityName");
-    });
+
+    for (i = 0; i < cities.length; i++) {
+        //
+        var listEl = $("<li>");  
+        $(listEl).append($("<button class='btn btn-info d-flex flex-column'>").attr("cityName", cities[i]).text(cities[i]));
+        $("#history").append(listEl);
+    }
+    
 };
 
 //function to create url based on search type
@@ -71,6 +67,57 @@ function createBreweryURL(){
     return url; 
 }
 
-console.log("Entered JS");
+function renderResults(response){
+
+}
+
+function callBrewAPI(){
+    //let city ="Denver" //$("#city-input").val().trim();
+    if (city !== "") {
+        // The following clears the error if something is typed in the search field that isn't accepted
+       $("#city-input").html("")
+        
+        // console.log(localStorage.getItem("city"));
+        // searchCity(city);
+        // brewery(city);
+        
+
+        $.ajax({
+            url: createBreweryURL(),
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            renderResults(response);
+        })
+    
+    }
+
+    ////////////////////////////////////This is not working
+    else {
+        $("#city-input").html("Field cannot be empty");
+    }
+}
+
+$("#history").on("click",function(){
+    event.preventDefault();
+    city = event.target.innerText;
+    console.log("clicked city= "+city);
+    //render search results
+    
+});
+
+$("#search").on("click", function() {
+    console.log("here")
+    event.preventDefault();
+    event.stopPropagation();
+
+    searchType = $("#select").val();
+    city = $("#findtext").val().trim();
+   
+    addHistory(city);
+    callBrewAPI();    
+});
+
+renderHistory();
 
 
